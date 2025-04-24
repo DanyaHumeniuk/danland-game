b8cf339198cb4b6de9b8c5b78758161e20944680
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-04-23 23:58:25 -0700

    Added map and title

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import map from "../assets/map.jpg.avif"
+
+const Hero = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="w-1/2 aspect-square lg:w-[75vh] lg:h-[75vh] rounded-xl flex justify-center items-center">
+        <img src={map} alt="MAP" className="h-full rounded-xl" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="relative lg:top-12 top-32">
+        <div className="text-white flex items-center justify-center text-4xl lg:text-6xl font-thin tracking-tight">
+            Welcome to DanLand!
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
