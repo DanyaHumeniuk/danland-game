ac49328eeec8c4cb22642502bca7d527bc9b9052
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-04-26 16:25:59 -0700

    Saved local changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-
+import { motion } from "framer-motion"
 
 const Navbar = () => {
   return (
     <div className="relative lg:top-12 top-32">
-        <div className="text-white flex items-center justify-center text-4xl lg:text-6xl font-thin tracking-tight">
+        <motion.div className="text-white flex items-center justify-center text-4xl lg:text-6xl font-thin tracking-tight">
             Welcome to DanLand!
-        </div>
+        </motion.div>
     </div>
   )
 }
