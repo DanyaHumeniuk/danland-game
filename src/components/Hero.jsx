04474914a8d3065ec5938f63b0d0a63ac7729b40
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-04-26 16:02:12 -0700

    Update Hero.jsx with new background and points

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,23 @@ import map from "../assets/map.jpg"
 const Hero = () => {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <div className="w-1/2 aspect-square lg:w-[75vh] lg:h-[75vh] rounded-xl flex justify-center items-center">
-        <img src={map} alt="MAP" className="h-full rounded-xl" />
-      </div>
+        <div className="relative w-full max-w-lg aspect-square rounded-xl bg-cover bg-center" style={{ backgroundImage: `url(${map})` }}>
+            <div className="absolute top-[17%] left-[27%] flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full bg-white text-black font-bold text-xs sm:text-sm">
+            A
+            </div>
+            <div className="absolute top-[51%] left-[17%] flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full bg-white text-black font-bold text-xs sm:text-sm">
+            B
+            </div>
+            <div className="absolute top-[23%] left-[62%] flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full bg-white text-black font-bold text-xs sm:text-sm">
+            C
+            </div>
+            <div className="absolute top-[81%] left-[35%] flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full bg-white text-black font-bold text-xs sm:text-sm">
+            D
+            </div>
+            <div className="absolute top-[69%] left-[80%] flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full bg-white text-black font-bold text-xs sm:text-sm">
+            E
+            </div>
+        </div>
     </div>
   )
 }
