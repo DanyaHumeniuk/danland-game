07ce1b344b9f59069b8e79b133d788dd56b0b896
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-04-26 21:26:15 -0700

    Fixed the navbar text sizing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 const Navbar = () => {
   return (
     <div className="relative top-32">
-        <motion.div className="text-white flex items-center justify-center text-4xl font-thin tracking-tight">
+        <motion.div className="text-white flex items-center justify-center text-2xl md:text-4xl sm:text-3xl font-thin tracking-tight">
             Welcome to DanLand!
         </motion.div>
     </div>
